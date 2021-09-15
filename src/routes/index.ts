ea3d0860787fd06b4b7ada48da928acec4724096
Manoel Martins <mano.martins29@gmail.com>
2021-09-15 14:47:27 -0300

    feat: able to create a new user

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,11 @@ import { Router } from "express";
 
 import categoriesRouter from "./categories.routes";
 import specificationRouter from "./specifications.routes";
+import usersRouter from "./users.routes";
 
 const router = Router();
 
+router.use("/users", usersRouter);
 router.use("/categories", categoriesRouter);
 router.use("/specifications", specificationRouter);
 
