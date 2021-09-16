5860c1bbcc9779b72635962b37e14f669bdcd3e2
Manoel Martins <mano.martins29@gmail.com>
2021-09-16 11:09:55 -0300

    refactor

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import multer from "multer";
 
-import upload from "../config/upload";
-import { ensureAuthentication } from "../middlewares/ensureAuthenticated";
-import CreateUserController from "../modules/accounts/useCases/createUser/CreateUserController";
-import UpdateUserAvatarController from "../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
+import upload from "@config/upload";
+import CreateUserController from "@modules/accounts/useCases/createUser/CreateUserController";
+import UpdateUserAvatarController from "@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
+import { ensureAuthentication } from "@shared/infra/http/middlewares/ensureAuthenticated";
 
 const uploadAvatar = multer(upload.upload("./tmp/avatar"));
 
