c5d987c039bc801d7a8233d552f09334ea85d885
Manoel Martins <mano.martins29@gmail.com>
2021-09-27 20:48:27 -0300

    chore: able to create car

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import authenticateRouter from "./authenticate.routes";
+import carsRoutes from "./cars.routes";
 import categoriesRouter from "./categories.routes";
 import specificationRouter from "./specifications.routes";
 import usersRouter from "./users.routes";
@@ -11,5 +12,6 @@ router.use(authenticateRouter);
 router.use("/users", usersRouter);
 router.use("/categories", categoriesRouter);
 router.use("/specifications", specificationRouter);
+router.use("/cars", carsRoutes);
 
 export default router;
